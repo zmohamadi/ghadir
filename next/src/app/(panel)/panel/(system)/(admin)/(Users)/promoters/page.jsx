1421d021dfc3d260564d46919d603be366d8d3e1
zmohamadi <zmohamadi44@yahoo.com>
2024-12-01 15:11:34 +0430

    login--

--- next/src/app/(panel)/panel/(system)/(admin)/(Users)/promoters/page.jsx
+++ next/src/app/(panel)/panel/(system)/(admin)/(Users)/promoters/page.jsx
@@ -3,9 +3,9 @@ import { List } from "@/Components/Promoters/List";
 import { useAuth } from "@/lib";
 
 export default function page(){
-    const { user } = useAuth({ guard: "admin" });
-    console.log("user");
-    console.log(user);
+    const { user } = useAuth();
+    // console.log("user");
+    // console.log(user);
     const panel = user?.role_id == 1 ?  "admin" : "promoter";
     const access = user?.role_id == 1 ?  true : false;
     // const query = user?.role_id == 2 &&`register_status=1`;
