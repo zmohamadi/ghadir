fd2e0018ba294758f82687f6c212f07e4e0a5668
MEr1992 <m.ebadi.r@gmail.com>
2024-12-08 00:17:24 +0330

    changes Gender 1403/09/18 00:17

--- next/src/Components/Promoters/CulturalUsers.jsx
+++ next/src/Components/Promoters/CulturalUsers.jsx
@@ -5,8 +5,10 @@ import { Input, Textarea, Radio } from "@/Theme/Midone/Forms";
 
 export function CulturalUsers({ index, parent, addIcon, closeIcon,needles }) {
     const { Lang,local } = useLang();
-    const culturalUser = parent?.state?.info?.cultural_users?.[index];
+    let culturalUser = parent?.state?.info?.cultural_users?.[index];
 
+    console.log("needles?.gender");
+    console.log("c_gender_id_"+index+"_"+needles?.gender?.length+"_"+culturalUser?.gender_id);
     return (<>
     <div className="col-span-12 flex justify-end items-end">
                 {closeIcon}
@@ -17,10 +19,12 @@ export function CulturalUsers({ index, parent, addIcon, closeIcon,needles }) {
             <Input label="job_position" refItem={[parent, `c_job_position_${index}`]} defaultValue={culturalUser?.job_position}  />
             <Input label="phone" refItem={[parent, `c_phone_${index}`]} defaultValue={culturalUser?.phone}  />
             <Textarea label="description" refItem={[parent, `c_comments_${index}`]} defaultValue={culturalUser?.comments}  />
-            <Radio defaultValue={culturalUser?.gender_id}   type="col" label="gender" id="c_gender_id" 
+            <Radio defaultValue={culturalUser?.gender_id} type="col" label="gender" 
                 refItem={[parent,`c_gender_id_${index}`]}
-                data={needles?.gender} titleKey={"title_"+local}  
-                key={"c_gender_id_"+index}
+                data={needles?.gender} titleKey={"title_"+local}
+                key={culturalUser?.gender_id}
+                // id="c_gender_id" 
+                // key={"c_gender_id_"+index}
             />
             <div className="col-span-12 flex justify-start items-start border-b-4 mt-2 mb-2">
                 {addIcon}
